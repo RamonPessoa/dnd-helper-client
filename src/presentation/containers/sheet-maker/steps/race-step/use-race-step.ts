e5b31ffde48f80ef8b039016adf6f2440982253b
RamonPessoa <reimund.programmer@gmail.com>
2023-05-01 17:21:09 -0300

    refactor: update race step to get race list

--- src/presentation/containers/sheet-maker/steps/race-step/use-race-step.ts
+++ src/presentation/containers/sheet-maker/steps/race-step/use-race-step.ts
@@ -1,26 +1,46 @@
+import { IRaceListItem } from "@/domain/usecases/race/get-race-list";
 import { useToast } from "@/presentation/components/toast";
+import { useRaceService } from "@/presentation/hooks/services/race-service";
+import { useCallback, useEffect, useState } from "react";
 
 interface UseNameStepProps {
   handleNextStep: () => void;
 }
 
-const useNameStep = ({ handleNextStep }: UseNameStepProps) => {
+const useRaceStep = ({ handleNextStep }: UseNameStepProps) => {
   const { ToastComponents, showToast } = useToast();
+  const [raceList, setRaceList] = useState<IRaceListItem[]>();
+  const { raceService } = useRaceService();
+
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const formValues = new FormData(event.currentTarget);
-    const name = formValues.get("race") as string;
-    if (!name) {
-      showToast("Name is required", "error", 3000);
+    const race = formValues.get("race") as string;
+    if (!race) {
+      showToast("race is required", "error", 3000);
       return;
     }
     handleNextStep();
   };
 
+  const fetchRaceList = useCallback(async () => {
+    const response = await raceService.remoteGetRaceList.getRaceList();
+    if (response.error) {
+      showToast(response.error, "error", 3000);
+      return;
+    }
+    if (response.body) setRaceList(response.body.results);
+  }, [raceService.remoteGetRaceList, showToast]);
+
+  useEffect(() => {
+    fetchRaceList();
+  }, [fetchRaceList]);
+
   return {
     handleSubmit,
     ToastComponents,
+    raceList,
   };
 };
 
-export default useNameStep;
+export default useRaceStep;
