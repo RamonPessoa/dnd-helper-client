b8c3e8cc15ce99024b5764c1a84937f40503f059
RamonPessoa <reimund.programmer@gmail.com>
2023-05-06 16:50:58 -0300

    refactor: remove unused imports in user page

--- src/pages/user/index.tsx
+++ src/pages/user/index.tsx
@@ -1,11 +1,7 @@
-import Avatar from "@/presentation/components/avatar/avatar";
-import Button from "@/presentation/components/button/button.";
-import Modal from "@/presentation/components/modal/modal";
 import SheetButton from "@/presentation/components/sheet-button/sheet-button";
 import SheetMaker from "@/presentation/containers/sheet-maker/sheet-maker";
 import SheetMakerContextProvider from "@/presentation/containers/sheet-maker/sheet-maker-context";
-import { useRaceService } from "@/presentation/hooks/services/race-service";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 export default function UserPage() {
   const [isSheetMakerOpen, setIsSheetMakerOpen] = useState(false);
