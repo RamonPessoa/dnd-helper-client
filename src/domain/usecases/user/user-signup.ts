d66a9d2fbcfa25281f01c35ee9dbcb1a7aec8aa0
RamonPessoa <reimund.programmer@gmail.com>
2023-05-06 16:47:56 -0300

    refactor: add an interface for userSignupResponse

--- src/domain/usecases/user/user-signup.ts
+++ src/domain/usecases/user/user-signup.ts
@@ -3,6 +3,14 @@ export interface UserSignupParams {
   password: string;
 }
 
+export interface UserSignupResponse {
+  id: string;
+  name: string;
+  password: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
 export interface UserSignup<R> {
   signup: (params: UserSignupParams) => Promise<R>;
 }
