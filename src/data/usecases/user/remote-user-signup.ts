f6b312fb7196e7eabfd047b6a4bf89f4ba89f740
RamonPessoa <reimund.programmer@gmail.com>
2023-05-06 16:48:53 -0300

    refactor: define the RemoteUserSignup response to be UserSignupResponse

--- src/data/usecases/user/remote-user-signup.ts
+++ src/data/usecases/user/remote-user-signup.ts
@@ -3,18 +3,23 @@ import {
   RemoteHttpPostParams,
   RemoteHttpPostResponse,
 } from "@/data/protocols/http/remote-http-post-client";
-import { UserSignup, UserSignupParams } from "@/domain/usecases/user/user-signup";
+import {
+  UserSignup,
+  UserSignupParams,
+  UserSignupResponse,
+} from "@/domain/usecases/user/user-signup";
 
-export default class RemoteUserSignup implements UserSignup<RemoteHttpPostResponse<any>> {
+export default class RemoteUserSignup
+  implements UserSignup<RemoteHttpPostResponse<UserSignupResponse>>
+{
   constructor(
     private readonly url: string,
-    private readonly httpClient: RemoteHttpPostClient<
-      UserSignupParams,
-      any
-    >
+    private readonly httpClient: RemoteHttpPostClient<UserSignupParams, UserSignupResponse>
   ) {}
 
-  async signup(params: UserSignupParams): Promise<RemoteHttpPostResponse<any>> {
+  async signup(
+    params: UserSignupParams
+  ): Promise<RemoteHttpPostResponse<UserSignupResponse>> {
     return this.httpClient.post({
       url: this.url,
       body: params,
